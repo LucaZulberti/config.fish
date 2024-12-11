1b1dd4242199d0d4ee82c71d93236d0a1ecd8ffa
Luca Zulberti <zulberti.luca@gmail.com>
2024-12-11 20:26:11 +0100

    :goal_net: Show error code in TMUX prompt

--- conf.d/prompt.fish
+++ conf.d/prompt.fish
@@ -9,10 +9,23 @@ end
 
 # Check if inside a tmux session
 if set -q TMUX
-    # Override the prompt
+    # Inside tmux: Add a custom indicator and skip original fish prompt
+
+    # Override the fish prompt
     function fish_prompt
-        # Inside tmux: Add a custom indicator and skip original fish prompt
+        # Capture the return value
+        set return_value $status
+        
         set_color cyan
-        echo -n "🐟 > "
+        echo -n "🐟 "
+        
+        # Print the prompt
+        if test $return_value -ne 0
+            set_color red
+            echo -n "[$return_value]"
+        end
+            
+        set_color cyan
+        echo -n " > "
     end
 end
