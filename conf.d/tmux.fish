374f9550091d152c4582c5e08435e596caa8d8fc
Luca Zulberti <zulberti.luca@gmail.com>
2024-12-11 09:27:24 +0100

    :recycle: Dedicate to tmuxp an improved configuration file

--- conf.d/tmux.fish
+++
@@ -1,4 +0,0 @@
-alias tmuxs "~/.config/tmux/scripts/tmux-sessionizer-tmuxp"
-
-# Link tmuxp sessions to tmuxp directory
-[ ! -e ~/.tmuxp ] && ln -s ~/.config/tmux/sessions ~/.tmuxp
---
+++ conf.d/tmuxp.fish
@@ -0,0 +1,4 @@
+set -x TMUXP_CONFIGDIR ~/.config/tmuxp/sessions
+
+# My tmuxp sessionizer
+alias tmuxs "~/.config/tmuxp/tmuxp-sessionizer"
