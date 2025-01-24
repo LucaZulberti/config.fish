c330869c9f036c94d7bbf1374777a00df4e6ac5a
Luca Zulberti <luca.zulberti@ingeniars.com>
2025-01-24 11:03:59 +0100

    :loud_sound: Use standard hostname prompt

--- conf.d/prompt.fish
+++ conf.d/prompt.fish
@@ -1,12 +1,3 @@
-# Override 'prompt_hostname'
-function prompt_hostname --description 'short hostname for the prompt'
-    # First letter uppercase anyway
-    set capitalized (string upper (string sub -l 1 $hostname))(string sub -s 2 $hostname) 
-
-    # Keep only capitalized letters in hostname
-    string match -ar '[A-Z]+' $capitalized | string join ''
-end
-
 # Check if inside a tmux session
 if set -q TMUX
     # Inside tmux: Add a custom indicator and skip original fish prompt
