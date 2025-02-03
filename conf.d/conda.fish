a1a6f4378706e4cd60822e8711998efa610dd793
Luca Zulberti <Luca.Zulberti@ingeniars.com>
2025-02-03 17:31:07 +0100

    :bug: Fix paths in conda.fish

--- conf.d/conda.fish
+++ conf.d/conda.fish
@@ -1,12 +1,12 @@
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if test -f /Users/luca/miniconda3/bin/conda
-    eval /Users/luca/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+if test -f ~/miniconda3/bin/conda
+    eval ~/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 else
-    if test -f "/Users/luca/miniconda3/etc/fish/conf.d/conda.fish"
-        . "/Users/luca/miniconda3/etc/fish/conf.d/conda.fish"
+    if test -f "~/miniconda3/etc/fish/conf.d/conda.fish"
+        . "~/miniconda3/etc/fish/conf.d/conda.fish"
     else
-        set -x PATH "/Users/luca/miniconda3/bin" $PATH
+        set -x PATH "~/miniconda3/bin" $PATH
     end
 end
 # <<< conda initialize <<<
