3f1011febb76a79383da1bae8079b1bacf6fb0a3
Luca Zulberti <Luca.Zulberti@ingeniars.com>
2025-02-03 17:32:14 +0100

    :bug: Source hererocks only if exists

--- conf.d/hererocks.fish
+++ conf.d/hererocks.fish
@@ -1,5 +1,7 @@
 # Installed Lua 5.1 with
 # hererocks .hererocks/5.1 -l5.1 -rlatest
 
-# Always source that environment
-source ~/.hererocks/5.1/bin/activate.fish
+if test -d ~/.hererocks
+  # Always source that environment
+  source ~/.hererocks/5.1/bin/activate.fish
+end
