f3bc3f155ab161428f621f94ae216b82ab2d9579
Luca Zulberti <zulberti.luca@gmail.com>
2025-03-03 16:56:52 +0100

    :bug: Fix use of 1Password SSH Agent in WSL

--- conf.d/1password.fish
+++ conf.d/1password.fish
@@ -5,8 +5,10 @@ end
 
 # 1Password SSH Agent
 if not set -q SSH_AUTH_SOCK
-  ln -sf ~/.1password/agent.sock ~/.agent.sock
-  set -x SSH_AUTH_SOCK ~/.agent.sock
+  if test -S ~/.1password/agent.sock
+    ln -sf ~/.1password/agent.sock ~/.agent.sock
+    set -x SSH_AUTH_SOCK ~/.agent.sock
+  end
 else
   ln -sf "$SSH_AUTH_SOCK" ~/.agent.sock
 end
--- conf.d/wsl.fish
+++ conf.d/wsl.fish
@@ -1,4 +1,7 @@
 if test -d /mnt/c/Users
-    alias ssh='ssh.exe'
-    alias ssh-add='ssh-add.exe'
+  alias ssh='ssh.exe'
+  alias ssh-add='ssh-add.exe'
+
+  # Use windows ssh in Git
+  git config --global core.sshCommand ssh.exe
 end
