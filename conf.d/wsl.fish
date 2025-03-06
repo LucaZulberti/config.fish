b82dba893a71676989aa9d4de3fede5267ceac3c
Luca Zulberti <zulberti.luca@gmail.com>
2025-03-06 14:21:18 +0100

    :bug: Copy .ssh directory from windows symlinked one .ssh_win

--- conf.d/wsl.fish
+++ conf.d/wsl.fish
@@ -1,7 +1,16 @@
 if test -d /mnt/c/Users
   alias ssh='ssh.exe'
   alias ssh-add='ssh-add.exe'
+  alias scp='scp.exe'
 
   # Use windows ssh in Git
   git config --global core.sshCommand ssh.exe
+
+  # Copy .ssh from Windows link, if present
+  if test -s ~/.ssh_win
+    rm -rf ~/.ssh
+    cp -rL ~/.ssh_win ~/.ssh
+    chmod -R 600 ~/.ssh/*
+    chmod    700 ~/.ssh
+  end
 end
