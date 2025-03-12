848d2e7d9f5e98e312f322b0cacd29642bb40488
Luca Zulberti <zulberti.luca@gmail.com>
2025-03-12 09:16:03 +0100

    :bug: Fix how 1Password conf sets SSH_AUTH_SOCK

--- conf.d/1password.fish
+++ conf.d/1password.fish
@@ -3,12 +3,19 @@ if type -q op
   op completion fish | source
 end
 
-# 1Password SSH Agent
-if not set -q SSH_AUTH_SOCK
+# If SSH Agent is not set or is using macOS Launchd socket...
+if not set -q SSH_AUTH_SOCK || string match -q "/private/tmp/com.apple.launchd.*" $SSH_AUTH_SOCK
+  # Use 1Password SSH Agent if available
   if test -S ~/.1password/agent.sock
-    ln -sf ~/.1password/agent.sock ~/.agent.sock
-    set -x SSH_AUTH_SOCK ~/.agent.sock
+    set -x SSH_AUTH_SOCK ~/.1password/agent.sock
+  else if test -S ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/ssh-agent.sock
+    set -x SSH_AUTH_SOCK ~/Library/Group\ Containers/2BUA8C4S2C.com.1password/ssh-agent.sock
   end
+end
+
+# Ensure ~/.agent.sock always links to the active SSH_AUTH_SOCK
+if set -q SSH_AUTH_SOCK
+  ln -sf $SSH_AUTH_SOCK ~/.agent.sock
 else
-  ln -sf "$SSH_AUTH_SOCK" ~/.agent.sock
+  rm -f ~/.agent.sock
 end
