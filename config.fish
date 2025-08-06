119c359e4b78bf7c720ef84a0f7b917d4506d4ec
Luca Zulberti <Luca.Zulberti@ingeniars.com>
2025-08-06 21:48:36 +0200

    :bug: Set TZ in fish only if not already set (workenv set it)

--- config.fish
+++ config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 
     # Set time zone
-    set -x TZ (readlink /etc/localtime | sed 's|.*/zoneinfo/||')
+    if not set -q TZ
+        set -x TZ (readlink /etc/localtime | sed 's|.*/zoneinfo/||')
+    end
 end
 
