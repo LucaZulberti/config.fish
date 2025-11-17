09b09547a87066ebfefe4fc5a67911e3d0481f14
Luca Zulberti <zulberti.luca@gmail.com>
2025-11-17 09:22:27 +0100

    [FIX] conf.d/wsl: Use ssh.exe as scp program and avoid to copy Windows config

--- conf.d/wsl.fish
+++ conf.d/wsl.fish
@@ -1,15 +1,8 @@
 if test -d /mnt/c/Users
-  alias ssh='ssh.exe'
-  alias ssh-add='ssh-add.exe'
-  alias scp='scp.exe'
+    alias ssh='ssh.exe'
+    alias ssh-add='ssh-add.exe'
+    alias scp='scp -S ssh.exe'
 
-  # Use windows ssh in Git
-  git config --global core.sshCommand ssh.exe
-
-  # Copy .ssh from Windows link, if present
-  if test -s ~/.ssh_win
-    rm -rf ~/.ssh/*
-    cp -rL ~/.ssh_win/* ~/.ssh
-    chmod -R 600 ~/.ssh/*
-  end
+    # Use windows ssh in Git
+    git config --global core.sshCommand ssh.exe
 end
