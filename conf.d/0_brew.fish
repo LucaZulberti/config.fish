c435969ca9c0c000343ff62bddb7a6d017bd2305
Luca Zulberti <zulberti.luca@gmail.com>
2026-04-11 00:12:35 +0200

    [UPDATE] conf.d/brew: Check also for brew on Linux

--- conf.d/0_brew.fish
+++ conf.d/0_brew.fish
@@ -1,3 +1,5 @@
 if test -d /opt/homebrew
     /opt/homebrew/bin/brew shellenv fish | source
+else if test -d /home/linuxbrew/.linuxbrew
+    /home/linuxbrew/.linuxbrew/bin/brew shellenv fish | source
 end
